794bb3220d17460030e2b8235a5266cb8f64a4bd
a5hi5hx <ashishpaudel54@gmail.com>
2023-07-26 13:51:14 +0545

    fix

--- api/admin.actions.js
+++ api/admin.actions.js
@@ -5,16 +5,46 @@ const User = require("../models/user.profile");
 const Product = require("../models/products");
 const Address = require("../models/address.book");
 const ScrollView = require('../models/scroll.view');
-router.post("/addScroll", async (req, res) => {
-    try {
-      const { title, pictures } = req.body;
-      const scrollView = new ScrollView({ title, pictures });
-      await scrollView.save();
-      res.status(201).json({ message: "Data added successfully", data: scrollView });
-    } catch (error) {
-      res.status(500).json({ error: "Failed to add data" });
-    }
+const multer = require("multer");
+const cloudinary = require("cloudinary"); // Replace with the actual path to your Cloudinary config file
+const ScrollView = require("./path/to/scrollViewModel"); // Replace with the actual path to your model file
+require("dotenv").config();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
   });
+// API route to add data with picture upload
+router.post("/add", upload.single("picture"), async (req, res) => {
+  try {
+    const { title } = req.body;
+    const picture = req.file;
+
+    if (!title || !picture) {
+      return res.status(400).json({ error: "Please provide title and a picture" });
+    }
+
+    // Upload the picture to Cloudinary and get the URL
+    const result = await cloudinary.uploader.upload(picture.buffer, {
+      folder: "scrollView_pictures", // Set your desired folder name on Cloudinary
+    });
+
+    // Save data with Cloudinary URL to MongoDB
+    const scrollView = new ScrollView({ title, pictures: [result.secure_url] });
+    await scrollView.save();
+
+    res.status(201).json({ message: "Data added successfully", data: scrollView });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add data" });
+  }
+});
+
+
   
   // API route to remove data by ID
   router.delete("/removeScroll/:id", async (req, res) => {
